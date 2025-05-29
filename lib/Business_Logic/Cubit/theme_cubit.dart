b375dbc10e2a6d2f0825b2d8f3b1564401e7ec39
hamdy haggag <hamdyhaggag74@gmail.com>
2025-05-29 03:05:45 +0300

    Initialize theme loading in ThemeCubit: Add _loadThemeFromCache method to retrieve and set the app's theme from cache upon initialization, enhancing user experience by preserving theme preference across sessions.

--- lib/Business_Logic/Cubit/theme_cubit.dart
+++ lib/Business_Logic/Cubit/theme_cubit.dart
@@ -2,11 +2,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:serat/imports.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(ThemeInitial());
+  ThemeCubit() : super(ThemeInitial()) {
+    _loadThemeFromCache();
+  }
 
   static ThemeCubit get(context) => BlocProvider.of(context);
 
   bool isLightMode = true;
+
+  void _loadThemeFromCache() {
+    final cachedTheme = CacheHelper.getBoolean(key: 'isLight');
+    if (cachedTheme != null) {
+      isLightMode = cachedTheme;
+      emit(ThemeChangeModeState());
+    }
+  }
 
   void changeAppMode({required bool? isLight}) {
     isLightMode = !isLightMode;
